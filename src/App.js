8395e3e4bc16a2bc8cd744c2b52570ec2186cb18
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-10 00:05:09 +0545

    Imported new component and routes fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Home from "./components/Home";
 import Project from "./components/project/project";
-import Registration from "./components/Registration"
+import Team from "./components/team/Team"
+import Dashboard from "./components/Dashboard";
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +15,8 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/projects" element={<Project />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
