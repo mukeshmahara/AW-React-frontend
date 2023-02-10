9c328f722148b89c72007fa8e1e5999a42294820
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-11 01:03:46 +0545

    Graph implementation in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../layouts/Layout'
-import { fetchWrapper } from '../_helper/fetchWrapper';
-import { ErrorMessageHandler } from '../_helper/_methods';
+import React from "react";
+import "chart.js/auto";
+import { Chart, Doughnut, Pie, PolarArea, Radar } from "react-chartjs-2";
 
 function Dashboard() {
-
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const getProjects = async()=>{
-      try {
-        let res = await fetchWrapper.get('projects');
-        setProjects(res.data);
-      } catch (error) {
-        ErrorMessageHandler(error)
-      }
-    }
-    getProjects();
-  }, [0]);
+  const lineData = {
+    labels: ["Jun", "Jul", "Aug",'sept','oct'],
+    datasets: [
+      {
+        id: 1,
+        label: "profit",
+        data: [25, 16, 17,15,24,22,11,48],
+      },
+      {
+        id: 2,
+        label: "loss",
+        data: [33, 20, 19,7,10,25,38,40],
+      },
+    ],
+  };
   return (
-    <div>
-      <Layout projects={projects}/>
+    <div className="my-4">
+      <div className="my-4 ">
+        <p className="mx-4 mb-2 text-5xl text-indigo-400">Dashboard</p>
+
+        <hr />
+      </div>
+
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <Chart type="line" data={lineData} />
+      </div>
+
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <Chart type="bar" data={lineData} />
+      </div>
+
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <Pie data={lineData} />
+      </div>
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <Doughnut data={lineData} />
+      </div>
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <PolarArea data={lineData} />
+      </div>
+      <div className="mx-2 my-3 cursor-pointer card justify-text hover:bg-gray-50">
+        <Radar data={lineData} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
