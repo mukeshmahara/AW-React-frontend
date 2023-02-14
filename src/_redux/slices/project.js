04a459eeb6f185f7ba3a2b293d632ecbf090190a
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-14 12:37:39 +0545

    Project Slice

---
+++ src/_redux/slices/project.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchWrapper } from "../../_helper/fetchWrapper";
+
+export const initialState = {
+  loading: false,
+  projects: [],
+  totalCount: 0
+};
+
+
+//A project slice
+const projectSlice = createSlice({
+  name:"projects",
+  initialState,
+  reducers:{
+    getProjects:(state,action)=>{
+      state.projects = action.payload
+      state.totalCount = action.payload.totalCount
+    }
+  }
+})
+
+//Action generated from slice
+const {
+  getProjects
+} = projectSlice.actions
+
+
+export const projectSelector = (state)=>state.projects;
+
+// export the reducers
+const projectReducer = projectSlice.reducer;
+
+export default projectReducer;
+
+// Actions 
+export const fetchProjects = (url) =>async(dispatch)=>{
+  try {
+    const response = await fetchWrapper.get(url);
+    console.log("helo",getProjects, response.data.length)
+    dispatch(getProjects({ projects: response.data, totalCount: response.data.length}))
+    
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+export const getProjectById = (project) =>async(dispatch)=>{
+
+}
