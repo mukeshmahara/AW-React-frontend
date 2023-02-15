91642f96de176733b2a9f76975b3d1a877deb006
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-15 16:04:46 +0545

    Added project and users reducers in root redeucer

--- src/_redux/slices/_index.js
+++ src/_redux/slices/_index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import projectReducer from "./project"
+import projectReducer from "./project";
+import userReducer from "./user";
 
-const rootReducer =combineReducers({
-  projects: projectReducer
+const rootReducer = combineReducers({
+  projects: projectReducer,
+  users: userReducer,
 });
 
 export default rootReducer;
