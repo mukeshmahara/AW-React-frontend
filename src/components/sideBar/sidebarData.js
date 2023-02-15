f8b1f168477f930e57466bea33b6ded85390654c
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-15 16:10:33 +0545

    SideBar data name and icon fixes

--- src/components/sideBar/sidebarData.js
+++ src/components/sideBar/sidebarData.js
@@ -1,59 +1,74 @@
-import { GrProjects, GrSettingsOption } from "react-icons/gr";
-import { BiBookContent, BiChat, BiHelpCircle, BiNotification, BiPhoneCall, BiUserPlus, BiVideo } from "react-icons/bi";
+import { AiOutlineProject } from "react-icons/ai";
+import {MdOutlineSpaceDashboard, MdOutlineSettings} from "react-icons/md";
+import {
+  BiBookContent,
+  BiChat,
+  BiHelpCircle,
+  BiNotification,
+  BiPhoneCall,
+  BiUserPlus,
+  BiVideo,
+} from "react-icons/bi";
 const sidebarData = [
   {
     id: 1,
+    name: "Dashboard",
+    icon: <MdOutlineSpaceDashboard fontSize={18} />,
+    link: "/admin",
+  },
+  {
+    id: 2,
     name: "Projects",
-    icon: <GrProjects fontSize={18} />,
-    link: "/projects",
+    icon: <AiOutlineProject fontSize={18} />,
+    link: "projects",
   },
   {
     id: 3,
     name: "Contents",
     icon: <BiBookContent fontSize={18} />,
-    link: "/contents",
+    link: "contents",
   },
   {
     id: 4,
     name: "Chat",
     icon: <BiChat fontSize={18} />,
-    link: "/chats",
+    link: "chats",
   },
   {
     id: 5,
     name: "Call",
     icon: <BiPhoneCall fontSize={18} />,
-    link: "/calls",
+    link: "calls",
   },
   {
     id: 6,
     name: "Video",
     icon: <BiVideo fontSize={18} />,
-    link: "/contents",
+    link: "contents",
   },
   {
     id: 7,
     name: "Peoples",
     icon: <BiUserPlus fontSize={18} />,
-    link: "/contents",
+    link: "contents",
   },
   {
     id: 8,
     name: "Setting",
-    icon: <GrSettingsOption fontSize={18} />,
-    link: "/settings",
+    icon: <MdOutlineSettings fontSize={18} />,
+    link: "settings",
   },
   {
     id: 9,
     name: "Help",
     icon: <BiHelpCircle fontSize={18} />,
-    link: "/contents",
+    link: "contents",
   },
   {
     id: 10,
     name: "FAQ",
     icon: <BiNotification fontSize={18} />,
-    link: "/contents",
+    link: "Faq",
   },
 ];
 
