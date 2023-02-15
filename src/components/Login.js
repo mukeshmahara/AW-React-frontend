0cef9dedd58c3695d24bc2bc16930365664f187d
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-15 16:12:23 +0545

    Login page issue fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { redirect, useNavigate } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { fetchWrapper } from "../_helper/fetchWrapper";
 import { ErrorMessageHandler } from "../_helper/_methods";
+import { userLogin } from "../_redux/slices/user";
 
 function Login() {
-
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -18,7 +17,7 @@ function Login() {
     formState: { errors },
   } = useForm({ mode: "onBlur", defaultValues: {} });
 
-  const onSubmit = async (data) => {
+  const handleLogin = async (data) => {
     console.log("data", data);
 
     let formData = {
@@ -29,91 +28,83 @@ function Login() {
     };
 
     try {
-      let res = await fetchWrapper.post("auth/signin/", formData);
-
-      toast.success("login successful", { position: "bottom-left" });
-      navigate("/dashboard");
+      dispatch(userLogin(formData, navigate));
     } catch (error) {
       ErrorMessageHandler(error);
     }
   };
 
   return (
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="w-full max-w-md space-y-8">
+    <div className="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
         <div>
           <img
-            class="mx-auto h-32 w-auto"
+            className="w-auto h-32 mx-auto"
             src="logo_transparent.png"
             alt="MK Technology"
           />
-          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          <h2 className="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">
             Sign in to your account
           </h2>
-          {/* <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            <a
-              href="#"
-              class="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              start your session
-            </a>
-          </p> */}
+          s
         </div>
-        <form class="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(handleLogin)}>
           <input type="hidden" name="remember" value="true" />
-          <div class="-space-y-px rounded-md shadow-sm">
+          <div className="-space-y-px rounded-md shadow-sm">
             <div>
-              <label for="email-address" class="sr-only">
+              <label htmlFor="email-address" className="sr-only">
                 Email address
               </label>
               <input
                 id="email-address"
                 name="email"
                 type="email"
-                autocomplete="email"
+                autoComplete="email"
                 {...register("email", { required: "Please enter your email." })}
                 required
-                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Email address"
               />
             </div>
             <div>
-              <label for="password" class="sr-only">
+              <label htmlFor="password" className="sr-only">
                 Password
               </label>
               <input
                 id="password"
                 name="password"
                 type="password"
-                autocomplete="current-password"
+                autoComplete="current-password"
                 {...register("password", {
                   required: "Please enter your password.",
                 })}
                 required
-                class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Password"
               />
             </div>
           </div>
 
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
               <input
                 id="remember-me"
                 name="remember-me"
                 type="checkbox"
-                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
               />
-              <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              <label
+                htmlFor="remember-me"
+                className="block ml-2 text-sm text-gray-900"
+              >
                 Remember me
               </label>
             </div>
 
-            <div class="text-sm">
+            <div className="text-sm">
               <a
                 href="#"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
               >
                 Forgot your password?
               </a>
@@ -123,21 +114,21 @@ function Login() {
           <div>
             <button
               type="submit"
-              class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 {/* <!-- Heroicon name: mini/lock-closed --> */}
                 <svg
-                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  className="w-5 h-5 text-indigo-500 group-hover:text-indigo-400"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </span>
