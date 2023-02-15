31141a58a54b7b0769e1f84d26f8cb1f75a337ac
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-15 16:03:55 +0545

    Protected Routes fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Project from "./components/project/Project";
-import Team from "./components/team/Team";
-import Dashboard from "./components/Dashboard";
-import UsersList from "./components/users/UsersList";
+
 import { Suspense } from "react";
 import Layout from "./layouts/Layout";
+import { ToastContainer } from "react-toastify";
+import { useSelector } from "react-redux";
+import admin from "./routes/admin";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.users.isLoggedin);
+  console.log(isLoggedIn, "login");
+  const user = useSelector((state) => state.users.user);
   return (
     <BrowserRouter>
       <Suspense fallback="Loading...">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="projects" element={<Project />} />
-            <Route path="team" element={<Team />} />
-            <Route path="users" element={<UsersList />} />
+          <Route path="projects" element={<Project />} />
+          <Route
+            path="/login"
+            element={isLoggedIn ? <Navigate to={"/admin"} /> : <Login />}
+          />
+
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute isLoggedIn={isLoggedIn}>
+                <Layout />
+              </PrivateRoute>
+            }
+          >
+            {admin}
           </Route>
         </Routes>
+        {/* <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        /> */}
+        <ToastContainer autoClose={2000} position="bottom-right" />
       </Suspense>
     </BrowserRouter>
   );
