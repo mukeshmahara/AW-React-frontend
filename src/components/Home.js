dda6f8c98350f135d558a180d82aca3555148c31
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-15 16:08:57 +0545

    Home page Refactored

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,148 +1,196 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {FaSignInAlt} from 'react-icons/fa'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSignInAlt } from "react-icons/fa";
+import { IoIosCreate, IoMdCreate } from "react-icons/io";
+import _header from "../layouts/header/_header";
+import _main from "../layouts/main/_main";
+import _footer from "../layouts/footer/_footer";
+import Project from "./project/Project";
 function Home() {
   return (
-    <div class="isolate bg-white">
-  <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-    <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
-      <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
-      <defs>
-        <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#9089FC"></stop>
-          <stop offset="1" stop-color="#FF80B5"></stop>
-        </linearGradient>
-      </defs>
-    </svg>
-  </div>
-  <div class="px-6 pt-6 lg:px-8">
-    <nav class="flex items-center justify-between" aria-label="Global">
-      <div class="flex lg:flex-1">
-        <Link to={"/"} class="-m-1.5 p-1.5">
-          <span class="sr-only">MK Software Solution</span>
-          <img class="h-32" src="logo_transparent.png" height="250px" alt=""/>
-        </Link>
-      </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span class="sr-only">Open main menu</span>
-          {/* <!-- Heroicon name: outline/bars-3 --> */}
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-      </div>
-      <div class="hidden lg:flex lg:gap-x-12">
-        <Link to={"/projects"} class="text-sm font-semibold leading-6 text-gray-900">Our Projects</Link>
-
-        <Link to={"#"} class="text-sm font-semibold leading-6 text-gray-900">Features</Link>
-
-        <Link to={"#"} class="text-sm font-semibold leading-6 text-gray-900">Marketplace</Link>
-
-        <Link to={"#"} class="text-sm font-semibold leading-6 text-gray-900">Company</Link>
-      </div>
-      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <Link to={"/login"} class="text-sm primary-button font-semibold leading-6 text-gray-900">sign in <span aria-hidden="true"><FaSignInAlt /> </span></Link>
-        <Link to={"/sign up"} class="text-sm font-semibold leading-6 text-gray-900">sign up <span aria-hidden="true">&rarr;</span></Link>
-      </div>
-      
-    </nav>
-    {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
-    <div role="dialog" aria-modal="true">
-      <div focus="true" class="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
-        <div class="flex items-center justify-between">
-          <Link to={"/login"} class="-m-1.5 p-1.5">
-            <span class="sr-only">MK Software Solution</span>
-            <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-          </Link>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-            <span class="sr-only">Close menu</span>
-            {/* <!-- Heroicon name: outline/x-mark --> */}
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <a href="#" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Product</a>
-
-              <a href="#" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Features</a>
-
-              <a href="#" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Marketplace</a>
-
-              <a href="#" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Company</a>
-            </div>
-            <div class="py-6">
-              <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <main>
-    <div class="relative px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
-        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-             <Link to={"/"} class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Join us <span aria-hidden="true">&rarr;</span></Link>
-          </div>
-        </div>
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">We ensure your success</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">We are the group of engineers, who cares about your optimal expense and lead you business online.</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <Link to={"#"} class="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</Link>
-            <Link to={"#"} class="text-base font-semibold leading-7 text-gray-900">Learn more <span aria-hidden="true">→</span></Link>
-          </div>
-        </div>
-      </div>
-      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-        <svg class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
-          <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+    <div className="bg-white isolate">
+      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+        <svg
+          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
           <defs>
-            <linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-              <stop stop-color="#9089FC"></stop>
-              <stop offset="1" stop-color="#FF80B5"></stop>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC"></stop>
+              <stop offset="1" stopColor="#FF80B5"></stop>
             </linearGradient>
           </defs>
         </svg>
       </div>
-    </div>
-  </main>
 
-  {/* <!-- This example requires Tailwind CSS v3.0+ --> */}
-<div class="bg-white">
-  <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-    <div class="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" class="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:translate-y-0 lg:-translate-x-1/2" aria-hidden="true">
-        <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
-        <defs>
-          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-            <stop stop-color="#7775D6"></stop>
-            <stop offset="1" stop-color="#E935C1"></stop>
-          </radialGradient>
-        </defs>
-      </svg>
-      <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-        <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boost your productivity.<br/>Start using our app today.</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
-        <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-          <a href="#" class="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
-          <a href="#" class="text-base font-semibold leading-7 text-white">Learn more <span aria-hidden="true">→</span></a>
+      <_header />
+
+      <_main />
+
+      <div>
+        {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+        {/* <div role="dialog" aria-modal="true">
+          <div
+            focus="true"
+            className="fixed inset-0 z-10 px-6 py-6 overflow-y-auto bg-white lg:hidden"
+          >
+            <div className="flex items-center justify-between">
+              <Link to={"/login"} className="-m-1.5 p-1.5">
+                <span className="sr-only">MK Software Solution</span>
+                <img
+                  className="h-8"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </Link>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="flow-root mt-6">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="py-6 space-y-2">
+                  <a
+                    href="#"
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-400/10"
+                  >
+                    Product
+                  </a>
+
+                  <a
+                    href="#"
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-400/10"
+                  >
+                    Features
+                  </a>
+
+                  <a
+                    href="#"
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-400/10"
+                  >
+                    Marketplace
+                  </a>
+
+                  <a
+                    href="#"
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-400/10"
+                  >
+                    Company
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+        {/* <!-- This example requires Tailwind CSS v3.0+ --> */}
+        <div className="bg-white">
+          <div className="py-24 mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8">
+            <div className="relative px-6 pt-16 overflow-hidden bg-gray-900 shadow-2xl isolate sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1024 1024"
+                className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:translate-y-0 lg:-translate-x-1/2"
+                aria-hidden="true"
+              >
+                <circle
+                  cx="512"
+                  cy="512"
+                  r="512"
+                  fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+                  fillOpacity="0.7"
+                />
+                <defs>
+                  <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                    <stop stopColor="#7775D6"></stop>
+                    <stop offset="1" stopColor="#E935C1"></stop>
+                  </radialGradient>
+                </defs>
+              </svg>
+              <div className="max-w-md mx-auto text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  Boost your productivity.
+                  <br />
+                  Start using our app today.
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Ac euismod vel sit maecenas id pellentesque eu sed
+                  consectetur. Malesuada adipiscing sagittis vel nulla.
+                </p>
+                <div className="flex items-center justify-center mt-10 gap-x-6 lg:justify-start">
+                  <a
+                    href="#"
+                    className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-base font-semibold leading-7 text-white"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+              <div className="relative mt-16 h-80 lg:mt-8">
+                <img
+                  className="absolute top-0 left-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                  src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                  alt="App screenshot"
+                  width="1824"
+                  height="1080"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-full aspect-[4/3] md:aspect-square">
+            <Project />
+          </div>
         </div>
       </div>
-      <div class="relative mt-16 h-80 lg:mt-8">
-        <img class="absolute top-0 left-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="App screenshot" width="1824" height="1080"/>
-      </div>
+      <_footer />
     </div>
-  </div>
-</div>
-
-</div>
-  )
+  );
 }
 
-export default Home
+export default Home;
